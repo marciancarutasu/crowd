45c41ec0690ae81c62c06a22446fb719b68fe8db
Marcian Carutasu <marciancarutasu@MacBook-Pro.local>
2020-07-26 13:07:52 +0300

    Wordpress Query in custom template file.

--- web/app/themes/go/likes.php
+++ web/app/themes/go/likes.php
@@ -9,24 +9,56 @@
             <h4>Displays the post with most likes.</h4>
             
             <?php 
+            /** 
+             * Get most liked post in query.
+             *
+             * @param integer $numberposts		The number of items
+             * @param array|string $post_type	Select post type
+             * @param string $method			keep this as default value (post, comment, activity, topic)
+             * @param string $period			Date peroid (all|today|yeterday|week|month|year)
+             * @param string $status			Log status (like|unlike|dislike|undislike)
+             * @return WP_Post[]|int[] 			Array of post objects or post IDs.
+             */
+            $the_query = wp_ulike_get_most_liked_posts( 10, array( 'post' ), 'post', 'all', 'like' );
 
-                /** 
-                 * Get most liked posts in query
-                 *
-                 * @param integer $numberposts		The number of items
-                 * @param array|string $post_type	Select post type
-                 * @param string $method			keep this as default value (post, comment, activity, topic)
-                 * @param string $period			Date peroid (all|today|yeterday|week|month|year)
-                 * @param string $status			Log status (like|unlike|dislike|undislike)
-                 * @return WP_Post[]|int[] 			Array of post objects or post IDs.
-                 */
-                $the_query = wp_ulike_get_most_liked_posts( 10, array( 'post' ), 'post', 'all', 'like' );
-
-                echo '<ul>';
+            echo '<ul>';
+            if($the_query) {
                 foreach($the_query as $post) {
                     echo '<li>' . $post->post_title . ' - ' . $post->post_date . '</li>';
                     echo '<p>' . $post->post_content . '</p>';
                 }
+            echo '</ul>';
+            } else {
+                echo '<p>No liked posts.</p>';
+            }
+            ?>
+
+            <h4>Displays all posts as list.</h4>
+
+            <?php
+            $args = array(
+                'post_type'  => 'post'
+            );
+
+            /** The Query sample usage,
+             * $args parameter can be customized for more complex db queries
+            **/
+            $the_query = new WP_Query( $args );
+            
+            // The Loop
+            if ( $the_query->have_posts() ) {
+                echo '<ul>';
+                while ( $the_query->have_posts() ) {
+                    $the_query->the_post();
+                    echo '<li>' . get_the_title() . '</li>';
+                }
+                echo '</ul>';
+            } else {
+                echo '<p>No posts found.</p>';
+            }
+
+            /* Restore original Post Data */
+            wp_reset_postdata();
             ?>
     </div>
 </div>
